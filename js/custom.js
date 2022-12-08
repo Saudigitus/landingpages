3517bc27b6410d214bfd9cce696f5e8cb0431e0a
AlziraXerinda <alziraxerinda0@gmail.com>
2022-12-08 15:05:03 +0200

    Updated layout

--- js/custom.js
+++ js/custom.js
@@ -1,33 +1,43 @@
-
-  (function ($) {
-  
+(function ($) {
   "use strict";
 
-    // COUNTER NUMBERS
-    jQuery('.counter-thumb').appear(function() {
-      jQuery('.counter-number').countTo();
-    });
-    
-    // CUSTOM LINK
-    $('.smoothscroll').click(function(){
-    var el = $(this).attr('href');
-    var elWrapped = $(el);
-    var header_height = $('.navbar').height();
+  // COUNTER NUMBERS
+  jQuery(".counter-thumb").appear(function () {
+    jQuery(".counter-number").countTo();
+  });
 
-    scrollToDiv(elWrapped,header_height);
+  // CUSTOM LINK
+  $(".smoothscroll").click(function () {
+    var el = $(this).attr("href");
+    var elWrapped = $(el);
+    var header_height = $(".navbar").height();
+
+    scrollToDiv(elWrapped, header_height);
     return false;
 
-    function scrollToDiv(element,navheight){
+    function scrollToDiv(element, navheight) {
       var offset = element.offset();
       var offsetTop = offset.top;
-      var totalScroll = offsetTop-navheight;
+      var totalScroll = offsetTop - navheight;
 
-      $('body,html').animate({
-      scrollTop: totalScroll
-      }, 300);
+      $("body,html").animate(
+        {
+          scrollTop: totalScroll,
+        },
+        300
+      );
     }
-});
-    
-  })(window.jQuery);
-
-
+  });
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
+})(window.jQuery);
